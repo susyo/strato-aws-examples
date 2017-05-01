3cc9c8ecd990cb887d36e42767c0d081bbf54a75
Omer Kushmaro <omer.kushmaro@stratoscale.com>
2017-05-01 13:16:34 +0300

    Basic ec2 java example

---
+++ java/aws-sdk-example/src/EC2Example.java
@@ -0,0 +1,38 @@
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
+
+public class EC2Example {
+
+    private static final String AMI_IDENTIFIER="<ami identifier>";
+    private static final String SYMPHONY_CLUSTER_ADDRESS="<cluster ip>";
+    private static AmazonEC2 ec2;
+
+    public static void main(String[] args) {
+
+        AWSCredentials credentials;
+        try {
+            credentials = new ProfileCredentialsProvider().getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load the credentials from the credential profiles file. " +
+                            "Please make sure that your credentials file is at the correct " +
+                            "location (~/.aws/credentials), and is in valid format.",
+                    e);
+        }
+
+        ec2 = new AmazonEC2Client(credentials);
+        ec2.setEndpoint(String.format("http://%s/api/v2/ec2/",SYMPHONY_CLUSTER_ADDRESS));
+
+        RunInstancesRequest runInstancesRequest = new RunInstancesRequest(AMI_IDENTIFIER,1,1);
+
+        RunInstancesResult result = ec2.runInstances(runInstancesRequest);
+
+        System.out.println(result.toString());
+    }
+
+}
