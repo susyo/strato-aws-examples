a27d5565deae3d2d7643ce5055a7509e2f0e5835
Omer Kushmaro <omer.kushmaro@stratoscale.com>
2017-12-31 18:00:07 +0200

    adding name tags

--- terraform/ec2-instance/simple-ec2.tf
+++ terraform/ec2-instance/simple-ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "ec2_instance" {
     ami = "${var.example_ami}"
 
+    tags{
+        Name="ec2"
+    }
     # Can use any aws instance type supported by symphony
     instance_type = "t2.micro"
 }
